453416a941922ad2a412548a58d347593e757b6a
Tom Rein <tomjrein@gmail.com>
2017-01-10 12:47:04 -0600

    add trailing commas

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -10,10 +10,8 @@ $environment_vars = [
   'OS_PROJECT_NAME=admin', 
   'OS_USER_DOMAIN_NAME=default', 
   'OS_PROJECT_DOMAIN_NAME=default', 
-  'OS_IDENTITY_API_VERSION=3' 
+  'OS_IDENTITY_API_VERSION=3'
 ]
-
-package { "python-pymysql": ensure => installed }
 
 class {'::mysql::server':
   package_name     => 'mariadb-server',
@@ -26,7 +24,7 @@ mysql::db { 'keystone':
   user     => 'keystone',
   host     => 'localhost',
   password => 'password',
-  grant    => ['SELECT', 'UPDATE']
+  grant    => ['SELECT', 'UPDATE'],
 }
 
 mysql_grant { 'keystone@localhost/*.*':
@@ -43,14 +41,14 @@ class {'::rabbitmq':
 
 rabbitmq_user { 'openstack':
   password => 'password',
-  provider => 'rabbitmqctl'
+  provider => 'rabbitmqctl',
 }
 
 rabbitmq_user_permissions { 'openstack@/':
   configure_permission => '.*',
   read_permission => '.*',
   write_permission => '.*',
-  provider => 'rabbitmqctl'
+  provider => 'rabbitmqctl',
 }
 
 file { '/vagrant/99-openstack.cnf':
@@ -58,64 +56,53 @@ file { '/vagrant/99-openstack.cnf':
   owner  => 'root',
   group  => 'root',
   path   => '/vagrant/99-openstack.cnf',
-  mode   => '0755'
+  mode   => '0755',
 }
 
 exec { 'cp-environment':
-  command => '/bin/cp /vagrant/environment /etc/environment'
+  command => '/bin/cp /vagrant/environment /etc/environment',
 }
 
 exec { 'cp-mysql-cnf':
   command => '/bin/cp /vagrant/99-openstack.cnf /etc/mysql/mariadb.conf.d/99-openstack.cnf',
-  require =>  [
-                File['/vagrant/99-openstack.cnf'],
-                Class['::mysql::server'],
-              ]
+  require =>  [ File['/vagrant/99-openstack.cnf'],
+                Class['::mysql::server'] ],
 }
 
 exec { 'restart_mysql':
   command => '/usr/sbin/service mysql restart',
-  require => Exec['cp-mysql-cnf'] 
-}
-
-exec { "install_keystone":
-  command => "/usr/bin/apt install -y keystone",
-  require => Class['::mysql::server'],
+  require => Exec['cp-mysql-cnf'],
 }
 
 file_line { 'edit_sql_connection':
   path  => '/etc/keystone/keystone.conf',
   line  => "connection = mysql+pymysql://keystone:${mysql_password}@controller/keystone",
   match => 'connection = sqlite:////var/lib/keystone/keystone.db',
-  require => Exec['install_keystone']
 }
 
 file_line { 'edit_token':
   path => '/etc/keystone/keystone.conf',
   line => "\nprovider = fernet",
   after => "^[token]$",
-  require => Exec['install_keystone']
 }
 
 exec { "populate_identity_database":
  command => 'keystone-manage db_sync',
  user    => 'keystone',
  path    => '/usr/bin',
- require =>  [ Exec['install_keystone'],
-               Exec['restart_mysql'],
+ require =>  [ Exec['restart_mysql'],
                Mysql_grant['keystone@localhost/*.*'],
-               Mysql::Db['keystone'],
-             ]
+               Mysql::Db['keystone'] ],
 }
 
 exec { "fernet_setup":
   command => '/usr/bin/keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone',
-  require => Exec['populate_identity_database']
+  require => Exec['populate_identity_database'],
 }
 
 exec { "credential_setup":
   command => '/usr/bin/keystone-manage credential_setup --keystone-user keystone --keystone-group keystone',
-  require => Exec['fernet_setup']
+  require => Exec['fernet_setup'],
 }
 
 exec { 'bootstrap_keystone':
@@ -124,50 +111,49 @@ exec { 'bootstrap_keystone':
              --bootstrap-internal-url http://controller:35357/v3/ \
              --bootstrap-public-url http://controller:5000/v3/ \
              --bootstrap-region-id RegionOne',
-  require => Exec['credential_setup']
+  require => Exec['credential_setup'],
 }
 
 file_line { 'add_controller_name':
- path => '/etc/apache2/apache2.conf',
- line => "\nServerName controller",
- before => Exec['restart_apache2'],
- require => Exec['install_keystone']
+  path => '/etc/apache2/apache2.conf',
+  line => "\nServerName controller",
+  before => Exec['restart_apache2'],
 }
 
 exec { 'restart_apache2':
   command => '/usr/sbin/service apache2 restart',
-  require => Exec['bootstrap_keystone']
+  require => Exec['bootstrap_keystone'],
 }
 
 exec { 'create_project':
   environment => $environment_vars,
   command => '/usr/bin/openstack project create --domain default \
              --description "Service Project" service',
-  require => Exec['restart_apache2']
+  require => Exec['restart_apache2'],
 }
 
 exec { 'create_demo_project':
   environment => $environment_vars,
   command => '/usr/bin/openstack project create --domain default \
              --description "Demo Project" demo',
-  require => Exec['restart_apache2']
+  require => Exec['restart_apache2'],
 }
 
 exec { 'create_demo_user':
   environment => $environment_vars,
   command => '/usr/bin/openstack user create --domain default --password password demo',
-  require => Exec['create_demo_project']
+  require => Exec['create_demo_project'],
 }
 
 exec { 'create_user_role':
   environment => $environment_vars,
   command => '/usr/bin/openstack role create user',
-  require => Exec['restart_apache2']
+  require => Exec['restart_apache2'],
 }
 
 exec { 'add_role_to_user':
   environment => $environment_vars,
   command => '/usr/bin/openstack role add --project demo --user demo user',
   require => [ Exec['create_demo_user'],
-               Exec['create_user_role'] ]
+               Exec['create_user_role'] ],
 }
